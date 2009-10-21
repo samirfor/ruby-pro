08d6bce615d3969630bb4371470ee3187ceffc7b
samirfor <samirfor@73d1711a-9c8a-11de-a1f4-ada230a207ef>
2009-10-21 21:41:12 +0000

    RS - iniciando projeto.

--- trunk/RS/src/rs/Html.java
+++ trunk/RS/src/rs/Html.java
@@ -4,8 +4,12 @@
  */
 package rs;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -45,6 +49,30 @@ public class Html {
         return false;
     }
 
+//    public boolean enviar(byte[] dados) throws IOException {
+//        OutputStream output = null;
+//        HttpConnection http = null;
+//        try {
+//            http = (HttpConnection) Connector.open(this.getHttpUrl(), Connector.READ_WRITE);
+//            http.setRequestMethod(HttpConnection.POST);
+//            http.setRequestProperty("Connection", "close");
+//            output = http.openOutputStream();
+//
+//            if (output != null) {
+//                output.write(dados);
+//                output.flush();
+//            }
+//            return ((http.getResponseCode() == HttpConnection.HTTP_OK));
+//        } finally {
+//            if (http != null) {
+//                http.close();
+//            }
+//            if (output != null) {
+//                output.close();
+//            }
+//        }
+//    }
+
     public String getBody() {
         StringBuilder stream = new StringBuilder();
         try {
@@ -70,5 +98,11 @@ public class Html {
 
     public String getLink() {
         return "http://" + uri.getHost() + uri.getPath();
+    }
+
+    public void toFile() throws FileNotFoundException, IOException {
+        File arq = new File("texto.html");
+        FileOutputStream fos = new FileOutputStream(arq);
+        fos.write(getBody().getBytes());
     }
 }
--- trunk/RS/src/rs/Main.java
+++ trunk/RS/src/rs/Main.java
@@ -4,8 +4,12 @@
  */
 package rs;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,7 +28,7 @@ public class Main {
         Html servidor = new Html();
         Scanner ler = new Scanner(System.in);
         String link, pag;
-        Pattern padraoServidor = Pattern.compile("\\S+$");
+        Pattern padraoServidor = Pattern.compile("http://rs\\d{1,3}\\S\\+.com", Pattern.MULTILINE);
         StringBuilder sb = new StringBuilder();
 
         System.out.println("::: Rapidshare V2 :::\n");
@@ -36,11 +40,19 @@ public class Main {
         if (pagina.modificaHost(link)) {
             System.out.println("Link mod: " + pagina.getLink());
 
-            pag = pagina.getBody();
-            Matcher fit = padraoServidor.matcher(pagina.getBody());
-            System.out.println(fit.matches());
+            try {
+                pagina.toFile();
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            System.out.println(pagina.getBody().indexOf("http://rs"));
+            System.out.println(pagina.getBody().substring(pagina.getBody().indexOf("http://rs"), (pagina.getBody().indexOf("http://rs"))+27));
 //            ArrayList<String> resultados = new ArrayList<String>();
 //            System.out.println(pagina.getBody().split(padrao)[0]);
+            // http://rs000.rapidshare.com
         }
     }
 }
