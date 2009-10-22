6458019246162dc58ebb6b8ba7e9a8aa1e97e727
samirfor <samirfor@73d1711a-9c8a-11de-a1f4-ada230a207ef>
2009-10-22 20:28:01 +0000

    RS - iniciando projeto.

--- RS/src/rs/Html.java
+++ RS/src/rs/Html.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package rs;
 
 import java.io.File;
@@ -9,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -17,18 +12,46 @@ import java.net.URLConnection;
 import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  *
- * @author multi
+ * @author Samir <samirfor@gmail.com>
  */
 public class Html {
 
     private URI uri;
+    private String body, link;
 
     public Html() {
     }
 
+    /**
+     * Cria a pagina com o host já alterado para o endereço IP.
+     * @param link - endereço completo do link.
+     */
+    public Html(String link) {
+        modificaHost(link);
+        this.body = getHTTPBody();
+        this.link = "http://" + uri.getHost() + uri.getPath();
+    }
+
+    /**
+     * Troca o hostname pelo endereço IP correspondente.
+     * Ex: rapidshare.com => 195.122.131.2
+     * 
+     * Os dados são guardados no atributo URI uri.
+     * @param link - endereço completo do link.
+     * @return
+     */
     public boolean modificaHost(String link) {
         try {
             uri = new URI(link);
@@ -49,31 +72,11 @@ public class Html {
         return false;
     }
 
-//    public boolean enviar(byte[] dados) throws IOException {
-//        OutputStream output = null;
-//        HttpConnection http = null;
-//        try {
-//            http = (HttpConnection) Connector.open(this.getHttpUrl(), Connector.READ_WRITE);
-//            http.setRequestMethod(HttpConnection.POST);
-//            http.setRequestProperty("Connection", "close");
-//            output = http.openOutputStream();
-//
-//            if (output != null) {
-//                output.write(dados);
-//                output.flush();
-//            }
-//            return ((http.getResponseCode() == HttpConnection.HTTP_OK));
-//        } finally {
-//            if (http != null) {
-//                http.close();
-//            }
-//            if (output != null) {
-//                output.close();
-//            }
-//        }
-//    }
-
-    public String getBody() {
+    /**
+     * Conecta-se ao site para obter o BODY da página.
+     * @return string - body da página.
+     */
+    public String getHTTPBody() {
         StringBuilder stream = new StringBuilder();
         try {
             URLConnection conexao = uri.toURL().openConnection();
@@ -88,6 +91,25 @@ public class Html {
         return stream.toString();
     }
 
+    /**
+     * Procura uma ocorrência de um padrão numa string.
+     * @return - a posição da string em que a substring está.
+     * - retorna -1 se não encontrar
+     */
+    public int buscaString(String padrao) {
+        int indice;
+        indice = body.indexOf(padrao);
+        return indice;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
     public String getHost() {
         return uri.getHost();
     }
@@ -97,12 +119,70 @@ public class Html {
     }
 
     public String getLink() {
-        return "http://" + uri.getHost() + uri.getPath();
+        return link;
     }
 
-    public void toFile() throws FileNotFoundException, IOException {
-        File arq = new File("texto.html");
-        FileOutputStream fos = new FileOutputStream(arq);
-        fos.write(getBody().getBytes());
+    public void setLink(String link) {
+        modificaHost(link);
+        this.link = "http://" + uri.getHost() + uri.getPath();
+    }
+
+    public URI getUri() {
+        return uri;
+    }
+
+    public void setUri(URI uri) {
+        this.uri = uri;
+    }
+
+    /**
+     * Escreve o atributo body numa arquivo.
+     * @param path - caminho do arquivo
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    public void toFile(String path) throws FileNotFoundException, IOException {
+        File arq = new File(path);
+        FileOutputStream stream = new FileOutputStream(arq);
+        stream.write(getBody().getBytes());
+    }
+
+    public String submit(String action, HashMap<String, String> dado) throws Exception {
+        StringBuilder stream = new StringBuilder();
+        URL url = new URL(action);
+        HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+        conexao.setRequestMethod("POST");
+        conexao.setDoOutput(true);
+        conexao.setDoInput(true);
+
+        DataOutputStream saida = new DataOutputStream(conexao.getOutputStream());
+
+        Set chaves = dado.keySet();
+        Iterator chaveIterador = chaves.iterator();
+        String conteudo = "";
+        for (int i = 0; chaveIterador.hasNext(); i++) {
+            Object key = chaveIterador.next();
+            if (i != 0) {
+                conteudo += "&";
+            }
+            conteudo += key + "=" + URLEncoder.encode(dado.get(key), "UTF-8");
+        }
+//        System.out.println(conteudo);
+        saida.writeBytes(conteudo);
+        saida.flush();
+        saida.close();
+
+        InputStream entrada = conexao.getInputStream();
+        byte[] buffer = new byte[1024];
+        while (entrada.read(buffer) != -1) {
+            stream.append(new String(buffer));
+        }
+
+        entrada.close();
+        return stream.toString();
+    }
+
+    public void substituirTudo(String padrao, String troca) {
+        body.replaceAll(padrao, troca);
     }
 }
