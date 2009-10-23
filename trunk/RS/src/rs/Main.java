dc29d2caf36e77d4569902e1916f656834d8c8ab
samirfor <samirfor@73d1711a-9c8a-11de-a1f4-ada230a207ef>
2009-10-23 03:00:35 +0000

    RS beta 1 - fazendo download pelo terminal OK

--- trunk/RS/src/rs/Main.java
+++ trunk/RS/src/rs/Main.java
@@ -10,6 +10,8 @@ import java.util.HashMap;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -20,13 +22,14 @@ public class Main {
     /**
      * @param args - link para download
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Html pagina, pagina2, postResp = null, download_link;
         Html rapidshare, ssl;
+        Pattern padrao;
+        Matcher pesquisa;
         Scanner ler = new Scanner(System.in);
         String link, servidor, download;
-        int primeiro_indice, ultimo_indice;
 
         System.out.println("::: Rapidshare V2 :::\n");
         System.out.println(">>> Criado por Samir <samirfor@gmail.com>\n");
@@ -39,51 +42,102 @@ public class Main {
             link = ler.next();
         }
 
-        // Identifica servidor
-        System.out.println("Conectando...");
-        ssl = new Html("http://ssl.rapidshare.com");
-        rapidshare = new Html("http://rapidshare.com");
-        pagina = new Html(link);
-        pagina.substituirTudo("http://rapidshare.com", "http://" + rapidshare.getHost());
-        pagina.substituirTudo("http://ssl.rapidshare.com", "http://" + ssl.getHost());
-        primeiro_indice = pagina.buscaString("http://rs");
-        ultimo_indice = primeiro_indice + 27;
-        servidor = pagina.getBody().substring(primeiro_indice, ultimo_indice);
-        System.out.println("Servidor identificado: " + servidor);
+        padrao = Pattern.compile("http://rapidshare.com/files/\\S+");
+        pesquisa = padrao.matcher(link);
+        if (pesquisa.find()) {
+            System.out.println("Obtendo: " + link);
 
-        // Envia form através de POST
-        System.out.println("Enviando requisição de download...");
-        pagina2 = new Html(servidor + pagina.getPath());
-        pagina2.substituirTudo("http://rapidshare.com", "http://" + rapidshare.getHost());
-        pagina2.substituirTudo("http://ssl.rapidshare.com", "http://" + ssl.getHost());
-        HashMap<String, String> hash = new HashMap<String, String>();
-        hash.put("dl.start", "Free");
-        try {
-            postResp = new Html();
-            postResp.setLink(pagina2.getLink());
-            System.out.println("Clicando no Free...");
-            postResp.setBody(pagina2.submit(pagina2.getLink(), hash));
-        } catch (Exception ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
+            // Identifica servidor
+            System.out.println("Conectando...");
+            ssl = new Html("https://ssl.rapidshare.com");
+            rapidshare = new Html("http://rapidshare.com");
+            pagina = new Html(link);
+            pagina.substituirTudo("http://rapidshare.com", "http://" + rapidshare.getHost());
+            pagina.substituirTudo("https://ssl.rapidshare.com", "https://" + ssl.getHost());
+            padrao = Pattern.compile("http://rs\\w+.rapidshare.com");
+            pesquisa = padrao.matcher(pagina.getBody());
+            if (pesquisa.find()) {
+                servidor = pesquisa.group();
+                System.out.println("Servidor identificado: " + servidor);
 
-        // Identificar link pra download
-        primeiro_indice = postResp.buscaString("document.dlf.action=\\\'http://rs") + 31;
-        ultimo_indice = primeiro_indice + 21;
-        download = "http://rs" + postResp.getBody().substring(primeiro_indice, ultimo_indice);
-        System.out.println("Link Download identificado: " + download + pagina.getPath());
-        download_link = new Html(download + pagina.getPath());
-        download_link.substituirTudo("http://rapidshare.com", "http://" + rapidshare.getHost());
-        download_link.substituirTudo("http://ssl.rapidshare.com", "http://" + ssl.getHost());
+                // Envia form através de POST
+                System.out.println("Enviando requisição de download...");
+                pagina2 = new Html(servidor + pagina.getPath());
+                pagina2.substituirTudo("http://rapidshare.com", "http://" + rapidshare.getHost());
+                pagina2.substituirTudo("https://ssl.rapidshare.com", "https://" + ssl.getHost());
+                HashMap<String, String> hash = new HashMap<String, String>();
+                hash.put("dl.start", "Free");
+                try {
+                    postResp = new Html();
+                    postResp.setLink(pagina2.getLink());
+                    System.out.println("Clicando no Free...");
+                    postResp.setBody(pagina2.submit(pagina2.getLink(), hash));
+                    postResp.substituirTudo("http://rapidshare.com", "http://" + rapidshare.getHost());
+                    postResp.substituirTudo("https://ssl.rapidshare.com", "https://" + ssl.getHost());
+                } catch (Exception ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
 
-        // Grava resultado no arquivo
-        try {
-            pagina2.toFile("download_link.html");
-            System.out.println("Resultado foi salvo no arquivo.");
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                padrao = Pattern.compile("Or try again in about \\d+");
+                pesquisa = padrao.matcher(postResp.getBody());
+                if (pesquisa.find()) {
+                    int tempo = Integer.parseInt(pesquisa.group().substring(22));
+                    System.out.println("Você baixou recentemente um arquivo. Aguardando" + tempo + " minutos.");
+                    Thread.sleep(tempo*60*1000);
+                }
+
+                // Identificar link pra download
+                padrao = Pattern.compile("http://rs\\S+\\\\");
+                pesquisa = padrao.matcher(postResp.getBody());
+                if (pesquisa.find()) {
+                    download = pesquisa.group().replace("\\", "");
+                    System.out.println("Link Download identificado: " + download);
+                    // Captura tempo de espera
+                    System.out.println("\nCapturando tempo de espera:");
+                    padrao = Pattern.compile("var c=\\d+");
+                    pesquisa = padrao.matcher(postResp.getBody());
+                    if (pesquisa.find()) {
+                        String tempo = pesquisa.group().substring(6);
+                        for (int i = Integer.parseInt(tempo) + 1; i >= 0; i--) {
+                            System.out.println("Resta " + i + " segundos.");
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException ie) {
+                            }
+                        }
+                        postResp.substituirTudo(pesquisa.group(), "var c=0");
+                    } else {
+                        System.out.println("Não foi possível capturar o tempo.");
+                    }
+
+                    download_link = new Html();
+                    download_link.setLink(download);
+                    System.out.println("Donwload mod: " + download_link.getLink());
+
+                    // Download por wget
+                    try {
+                        Runtime.getRuntime().exec("wget " + download_link.getLink() + " &");
+                    } catch (IOException ex) {
+                        Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    // Grava resultado no arquivo
+                    try {
+                        postResp.toFile("RapidDownload.html");
+                        System.out.println("Resultado foi salvo no arquivo.");
+                    } catch (FileNotFoundException ex) {
+                        Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IOException ex) {
+                        Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                } else {
+                    System.out.println("Não foi possível capturar o link de download.");
+                }
+            } else {
+                System.out.println("Servidor não identificado.");
+            }
+        } else {
+            System.out.println("Link inválido.");
         }
     }
 }
