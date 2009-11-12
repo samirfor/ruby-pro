9d9151362c98602caf1cb0626f0075f39f7a91da
samirfor <samirfor@73d1711a-9c8a-11de-a1f4-ada230a207ef>
2009-11-12 23:07:30 +0000

    Ruby: rapid atualizado

--- trunk/testaConexao/testaConexao.rb
+++ trunk/testaConexao/testaConexao.rb
@@ -4,19 +4,40 @@
 #############       Precisa ser executado como root
 
 require 'tk'
+require 'autenticaMD5.rb'
+require 'stop_resume.rb'
 
 # Configurações
-intervalo = 5      # tempo em minutos
-delay = 30         # tempo em segundos
-tentativa = 1 # iniciando número de tentativas de restabelecer a conexão
+intervalo = 10      # tempo em minutos
+delay = 0          # tempo em segundos
+tentativa = 10      # iniciando número de tentativas de restabelecer a conexão
+$ip_router = "10.0.0.1"
+$ip_cliente = "10.0.0.210"
+$pwd = `pwd`.chomp
 $reiniciar_apos = 0 # num de tentativas antes de reiniciar. zero -> desligado
-$desligar_apos = 5 # num de tentativas antes de desligar. zero -> desligado
+$desligar_apos = 20 # num de tentativas antes de desligar. zero -> desligado
 
 # Testa usuario
 if `whoami`.chomp != "root"
   puts "Este programa só funciona perfeitamente se o usuário tiver privilégios de administrador.\n\nTente    \"sudo ./testaConexao.rb\""
   `zenity --error --title='Testa Conexão!' --text='Este programa só funciona perfeitamente se o usuário tiver privilégios de administrador.\n\n<b>Tente    \"sudo ./testaConexao.rb\" </b>'`
   exit
+end
+
+def to_log(texto)
+  logger = Logger.new('testaConexao.log', 10, 1024000)
+  logger.datetime_format = "%d/%m %H:%M:%S"
+  logger.info(texto)
+  logger.close
+  puts texto
+end
+
+def to_log2(texto)
+  logger = Logger.new('testaConexaoCompleto.log', 10, 2048000)
+  logger.datetime_format = "%d/%m %H:%M:%S"
+  logger.info(texto)
+  logger.close
+  puts texto
 end
 
 # Métodos
@@ -38,63 +59,69 @@ end
 
 # Testa roteador
 def router_off?
-  puts "Testando conexão com o roteador ..."
-  ping = `ping 10.0.0.1 -c 1`
-  ping = ping.to_a.each {|x| x.chomp!}
-  ping = ping[1].gsub("From 10.0.0.210 icmp_seq=1 Destination ","")
-  if ping == "Net Prohibited" or ping == "Host Prohibited"
-    return false
-  end
-  if ping == "Net Unreachable" or ping == "Host Unreachable"
+  to_log2("Testando conexão com o roteador ...")
+  ping = `ping #{$ip_router} -c 2`
+  if ping == ""
     return true
+  else
+    ping = ping.scan(/Host Unreachable/)[0]
+    if ping == nil
+      to_log2("OK!")
+      return false
+    else
+      return true
+    end
   end
-  return false
 end
 
-def conexao_ok(intervalo)
-  t = Time.now
-  puts "\nConexão OK em: \t"+t.strftime("%d/%m/%Y %H:%M:%S %A")+
-    "\nProxima verificacao em "+intervalo.to_s+" minutos.\n\n"
+# Testa autenticação
+def autenticado
+  to_log2("Testando autenticação HotSpot ...")
+  ping = `ping google.com -c 1`
+  ping = ping.scan(/Net Prohibited/)[0]
+  if ping != nil
+    autentica # autenticarMD5.rb
+    sleep(10)
+    stop # JDownloader
+    resume # JDownloader
+    return true
+  else
+    to_log2("OK! Já está autenticado.")
+    return true
+  end
+end
+
+def conexaoOk(intervalo)
+  to_log2("Conexão OK.")
   sleep(intervalo*60)
 end
 
-def to_log(texto)
-  puts texto
-  arq = File.open( "testaConexao.log", "a" )
-  arq.puts(texto)
-  arq.flush
-  arq.close
-end
-
 def reiniciar
-  t = Time.now
-  to_log("Requisitou reinicialização em: \t"+t.strftime("%d/%m/%Y %H:%M:%S %A"))
+  to_log("Requisitou reinicialização.")
   system('shutdown -r 1 &')
   janela("reiniciará")
 end
 
 def desligar
-  t = Time.now
-  to_log("Requisitou desligamento em: \t"+t.strftime("%d/%m/%Y %H:%M:%S %A"))
+  to_log("Requisitou desligamento.")
   system('shutdown -h 1 &')
   janela("desligará")
 end
 
-def conexao_falhou(tentativa)
+def conexaoFalhou(tentativa)
   if tentativa == $reiniciar_apos
     reiniciar
   else
     if tentativa == $desligar_apos
       desligar
     else
-      t = Time.now
-      to_log("Tentativa de reconexão #"+tentativa.to_s+" em: \t"+t.strftime("%d/%m/%Y %H:%M:%S %A"))
+      to_log("Tentativa de reconexão #"+tentativa.to_s)
       return 1
     end
   end
 end
 
-def tenta_reconexao
+def tentaReconexao
   puts `# Bloco de comandos bash
      sudo /etc/init.d/NetworkManager stop    # Pára
      sudo modprobe -r rt61pci      # Desativa módulo
@@ -109,38 +136,35 @@ end
 #Fluxo principal
 puts "Aguardando delay pré-definido de "+delay.to_s+" segundos para iniciar..."
 sleep(delay) # tempo para os processos iniciarem
+tentativa = 1
 
 while true
-  if router_off?
-    t = Time.now
-    to_log("Roteador está offline. Tentando novamente em 10s...\t"+t.strftime("%d/%m/%Y %H:%M:%S %A"))
-    sleep(10)
-    desligar if router_off?
+  while router_off?
+    to_log("Roteador está offline. Tentando novamente...")
+    to_log2("Tentando reconexão...")
+    tentativa += conexaoFalhou(tentativa)
+    tentaReconexao
+    puts "Aguardando..."
+    sleep(60)
   end
-  pre_request = system('ping google.com -c 1')
-  if pre_request == true
-    conexao_ok(intervalo)
+  begin
+    resposta = autenticado
+  end while !resposta
+  request1 = system('ping yahoo.com -c 5')
+  if request1 == true
+    tentativa = 1
+    conexaoOk(intervalo)
   else
-    request1 = system('ping www.google.com -c 5')
-    if request1 == true
+    request2 = system('ping google.com -c 5')
+    if request2 == true
       tentativa = 1
-      conexao_ok(intervalo)
+      conexaoOk(intervalo)
     else
-      request2 = system('ping www.yahoo.com -c 5')
-      if request2 == true
-        tentativa = 1
-        conexao_ok(intervalo)
-      else
-        tentativa += conexao_falhou(tentativa)
-        puts "Tentando reconexão...\n\n"
-        tenta_reconexao
-        sleep(10)
-        system('nice -n 10 firefox www.google.com &')
-        #begin
-        #     codigo = 200
-        #end while codigo != 200 or codigo < 500 # Garante que a repetição não se dará por erro do servidor
-        sleep(30)
-      end
+      tentativa += conexaoFalhou(tentativa)
+      to_log2("Tentando reconexão...")
+      tentaReconexao
+      puts "Aguardando..."
+      sleep(60)
     end
   end
 end
