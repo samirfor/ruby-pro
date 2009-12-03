d17964c968de22c1f5f6f35aa5c3ee85b7618c77
samirfor <samirfor@73d1711a-9c8a-11de-a1f4-ada230a207ef>
2009-12-03 20:55:33 +0000

    Atualizando

--- trunk/download/lib/rss_parser/rss_parser.rb
+++ trunk/download/lib/rss_parser/rss_parser.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/env ruby
 
-require 'rss/1.0'
-require 'rss/2.0'
-require 'open-uri'
+require 'rss'
+
 
 def file_put_a(arquivo, array)
   arq = File.open(arquivo, 'a')
@@ -14,24 +13,16 @@ end
 
 def file_to_a(arquivo)
   if not FileTest.exist?(arquivo)
-    puts "Arquivo #{arquivo} não existe."
+    puts "ERRO: Arquivo #{arquivo} não existe."
     exit(1)
   end
   arq = File.open(arquivo, "r")
   array = Array.new
   arq.each_line do |linha|
-    array.push(linha.chomp)
+    array.push(linha.chomp) if not linha =~ /#.*/
   end
   arq.close
   array
-end
-
-def parse(fonte)
-  #exemplo fonte => "http://www.onelinkmoviez.com/feeds/posts/default?alt=rss"
-  content = ""
-  open(fonte) do |s| content = s.read end
-  rss = RSS::Parser.parse(content, false)
-  rss
 end
 
 def ja_inserido(link)
@@ -52,7 +43,7 @@ def browser(host, path)
     end
     puts res.body
   rescue Timeout::Error
-    puts("Tempo de requisição esgotado. Tentando novamente.")
+    puts("ERRO: Tempo de requisição esgotado. Tentando novamente.")
     retry
   rescue Exception => err
     STDERR.puts err
@@ -61,97 +52,152 @@ def browser(host, path)
   end
 end
 
-def run
-  puts "Carregando configurações do RSS."
-  rss_conf = file_to_a("rss.conf")
-  puts "Carregando lista de séries."
+def jdownloader_insert(links, jd_conf)
+  puts "\nINFO: Adicionando links ao JDownloader."
+  action = jd_conf[1] + "/action/add/links/grabber0/start1/?d="
+  jd_insert = Array.new
+  if FileTest.exist?("jd_historico")
+    jd_lista = file_to_a("jd_historico")
+    links.each do |link|
+      achou = false
+      jd_lista.each do |jd_link|
+        if link == jd_link
+          achou = true
+          next
+        end
+      end
+      if not achou
+        action += "%20" + link
+        jd_insert.push(link)
+      else
+        puts "INFO: #{link} já está no JD."
+      end
+    end
+  else
+    arq = File.open("jd_historico", "a")
+    arq.close
+    links.each do |link|
+      action += "%20" + link
+      jd_insert.push(link)
+    end
+  end
+  if jd_insert.length > 0
+    arq = File.open("jd_historico", 'a')
+    jd_insert.each do |a|
+      arq.print(a + "\n")
+    end
+    arq.close
+    browser(jd_conf[1], action)
+  else
+    puts "ERRO: Nenhum link foi adicionado no JD."
+    File.delete("lista")
+  end
+end
+
+def parse(fonte)
+  rss = nil
+  begin
+    rss = RSS::Parser.parse(fonte)
+  rescue RSS::InvalidRSSError
+    rss = RSS::Parser.parse(fonte, false)
+  rescue RSS::Error => err
+    STDERR.puts err
+    puts "ERRO: Erro no parse do RSS."
+    puts err
+    exit(1)
+  end
+  rss
+end
+
+# Escreve uma lista de links de interesse de acordo com o arquivo series.list
+# e adiciona ao JDownloader ou não.
+# server : String
+# add_jd : Boolean
+def run(server)
+  puts "INIT: Carregando configurações do RSS."
+  rss_conf = file_to_a("rss.conf")[1]
+  puts "INIT: Carregando configurações do JDownloader."
+  jd_conf = file_to_a("jdownloader.conf")
+  puts "INIT: Carregando lista de séries."
   series = file_to_a("series.list")
-  rss = parse(rss_conf[0])
-  
+  puts "INIT: Carregando lista de exceções."
+  excecoes = file_to_a("excecoes.list")
+  rss = parse(rss_conf)
+  puts "INIT: RSS versão: " + rss.version
   links = Array.new
-  puts "\nProcurando links...\n"
+
+  puts "\nINFO: Procurando links...\n"
   series.each do |s|
     rss.items.each do |f|
-      if f.title =~ /.*#{s}.*/i
-        puts "* " + f.title
+      if f.title =~ /.*#{s}.*(HDTV|DVDRip).*XviD.*/i
+        excecoes.each do |e|
+          if f.title =~ /.*#{e}.*/i
+            next
+          end
+        end
+        puts "+ " + f.title
         link = f.description
-        link = link.scan(/http:\/\/www.megaupload.com\S+\"/)[0].delete("\"")
-        if link == nil
-          puts "Erro: Não foi possível detectar o link de #{f.title}."
+        case server
+        when "www.megaupload.com" then
+          link = link.split("\"")
+        when "rapidshare.com" then
+          link = link.split("\n")
+        end
+        link = link.delete_if { |e| not (e =~ /.*http:\/\/#{server}.*/i) }
+        if link == nil or link.nitems == 0
+          puts "\nErro: Não foi possível detectar o(s) link(s) de #{f.title}."
           exit(1)
         else
-          links.push(link)
+          link.each do |lk|
+            links.push(lk)
+          end 
+          puts link, "\n"
           break
         end
       end
     end
   end
-  puts "Salvando lista de links."
+  puts "\nINFO: Salvando lista de links."
   if links.length > 0
     # Escreve a lista de links no final da lista de links
     file_put_a("lista", links)
-    puts "Lista salva com sucesso."
+    puts "INFO: Lista salva com sucesso."
   else
-    puts "Não há links se serem salvados."
+    puts "ERRO: Não há links se serem salvados."
   end
-  if ARGV[0] =~ /JD/i
-    puts "\nAdicionando links ao JDownloader."
-    unless ARGV[1] == nil
-      jdownloader = ARGV[1]
-      action = "/action/add/links/grabber0/start1/?d="
-      jd_insert = Array.new
-      if FileTest.exist?("jd_historico")
-        jd_lista = file_to_a("jd_historico")
-        links.each do |link|
-          achou = false
-          jd_lista.each do |jd_link|
-            if link == jd_link
-              achou = true
-              next
-            end
-          end
-          if not achou
-            action += "%20" + link
-            jd_insert.push(link)
-          else
-            puts "INFO: #{link} já está no JD."
-          end
-        end
-      else
-        arq = File.open("jd_historico", "a")
-        arq.close
-        links.each do |link|
-          action += "%20" + link
-          jd_insert.push(link)
-        end
-      end
-      if jd_insert.length > 0
-        arq = File.open("jd_historico", 'a')
-        jd_insert.each do |a|
-          arq.print(a + "\n")
-        end
-        arq.close
-        browser(ARGV[1], action)
-      else
-        puts "Nenhum link foi adicionado no JD."
-        File.delete("lista")
-      end
-    end
+  if jd_conf[3] == "ativado"
+    puts "INFO: JD ativado."
+    jdownloader_insert(links, jd_conf)
   else
-    puts "Especifique o host do JDownloader."
+    puts "INFO: JD desativado."
+  end
+end
+
+def main(server)
+  begin
+    run(server)
+  rescue Interrupt => err
+    STDERR.puts "\nINTERRUPT: Sinal de interrupção recebido"
+    puts "INTERRUPT: O programa foi encerrado."
+  rescue SystemExit => err
+    puts "INTERRUPT: O programa foi encerrado."
+  rescue Exception => err
+    STDERR.puts err
     exit(1)
   end
 end
 
-# O main do programa
-begin
-  run
-rescue Interrupt => err
-  STDERR.puts "\nSinal de interrupção recebido"
-  puts "O programa foi encerrado."
-rescue SystemExit => err
-  puts "O programa foi encerrado."
-rescue Exception => err
-  STDERR.puts err
-  exit(1)
+# Argumentos
+unless ARGV[0] == nil
+  if ARGV[0] =~ /MU/i
+    puts ">> Parse para Megaupload"
+    main("www.megaupload.com")
+  elsif ARGV[0] =~ /RS/i
+    puts ">> Parse para Rapidshare"
+    main("rapidshare.com")
+  else
+    puts "ERRO: Parâmetro inválido."
+    puts "\tColoque o servidor (RS ou MU) como parâmetro."
+    exit(1)
+  end
 end
