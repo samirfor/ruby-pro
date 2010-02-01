52aca8b7e49e48afa13dfd4712e6702f7c76546d
camurca.home <camurca.home@73d1711a-9c8a-11de-a1f4-ada230a207ef>
2010-02-01 18:25:44 +0000

    rs-online ~ Powered by eRuby.

---
+++ rs-online/lib/src/historico.rb
@@ -0,0 +1,12 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+class Historico
+  attr_writer :id, :data, :processo, :mensagem
+
+  def initialize(id, data, processo, mensagem)
+    @id = id
+    @data = data
+    @processo = processo
+    @mensagem = mensagem
+  end
+end
---
+++ rs-online/lib/src/historico_dao.rb
@@ -0,0 +1,26 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+require "dbi"
+
+class HistoricoDao
+  def initialize
+
+  end
+  
+  def find_all()
+    list = Array.new
+    conn = DBI.connect("DBI:Pg:postgres:localhost", "postgres", "postgres")
+    sql = <<FIMSQL
+    SELECT * FROM rs.historico ORDER BY data
+FIMSQL
+    rst = conn.execute(sql)
+    rst.fetch do |row|
+      historico = Historico::new(row["id"], row["data"], row["processo"], row["mensagem"])
+      p historico
+      list.push(historico)
+    end
+    rst.finish
+    conn.disconnect
+    return list
+  end
+end
