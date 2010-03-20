0b33f571153bea3f5523287207a6e23ce21d31b3
samirfor <samirfor@73d1711a-9c8a-11de-a1f4-ada230a207ef>
2010-03-20 22:16:15 +0000

    rs-online: twitter feature improved!

--- rs-online/lib/src/twitter.rb
+++ rs-online/lib/src/twitter.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'twitter'
 
 def tweet msg
+  puts "Twittando..."
   autenticacao = Twitter::HTTPAuth.new 'rsonline_', 'rs4all'
   cliente = Twitter::Base.new autenticacao
   cliente.update msg
