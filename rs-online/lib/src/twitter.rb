8a3e7c1f7a5851a8dd7bf6b8389e9564fde2d41a
samirfor <samirfor@73d1711a-9c8a-11de-a1f4-ada230a207ef>
2010-03-21 04:36:00 +0000

    rs-online: fix duplicated tweets

--- rs-online/lib/src/twitter.rb
+++ rs-online/lib/src/twitter.rb
@@ -5,5 +5,8 @@ def tweet msg
   puts "Twittando..."
   autenticacao = Twitter::HTTPAuth.new 'rsonline_', 'rs4all'
   cliente = Twitter::Base.new autenticacao
+  if cliente.user_timeline[0].text == msg
+    msg += "."
+  end
   cliente.update msg
 end
