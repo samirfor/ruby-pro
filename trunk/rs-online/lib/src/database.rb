2a22a861d13615f2ac7b1244f2f05daf10e3d4bf
samirfor <samirfor@73d1711a-9c8a-11de-a1f4-ada230a207ef>
2010-03-26 03:07:33 +0000

    rs-online: fix POO improved.

--- trunk/rs-online/lib/src/database.rb
+++ trunk/rs-online/lib/src/database.rb
@@ -214,6 +214,8 @@ def select_lista_links id_pacote
       link.tamanho = row["tamanho"]
       link.id_status = row["id_status"]
       link.testado = row["testado"]
+      link.data_inicio = row["data_inicio"]
+      link.data_fim = row["data_fim"]
       lista.push link
     end
     rst.finish
