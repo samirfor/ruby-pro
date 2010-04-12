4004229faff46bc21bff1f9a9317a78d40c651c4
samirfor <samirfor@73d1711a-9c8a-11de-a1f4-ada230a207ef>
2010-04-12 18:46:41 +0000

    Projeto sobre alocação dinâmica e ordenação de vetor em C.

---
+++ trunk/alocacao_dinamica/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=alocacao_dinamica
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/alocacao_dinamica
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=alocacaodinamica.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/alocacaodinamica.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=alocacao_dinamica
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/alocacao_dinamica
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=alocacaodinamica.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/alocacaodinamica.tar
