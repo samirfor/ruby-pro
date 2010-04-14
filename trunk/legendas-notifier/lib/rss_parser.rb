8c619803c12d4499f3140105d4011f3987239cab
samirfor <samirfor@73d1711a-9c8a-11de-a1f4-ada230a207ef>
2010-04-14 20:07:21 +0000

    legendas-notifier: some more tests

--- trunk/legendas-notifier/lib/rss_parser.rb
+++ trunk/legendas-notifier/lib/rss_parser.rb
@@ -1,35 +1,52 @@
 require 'rubygems'
-require 'feed_tools'
-require "sms_novo"
+require 'feed_tools' # gem install feedtools
+#require "sms_novo"
+require "sms"
 require "celular"
 
-feed = FeedTools::Feed.open('http://twitter.com/statuses/user_timeline/20011969.rss')
+puts "Abrindo RSS..."
 
-#puts feed.title
-#puts feed.link
-#puts feed.description
+def run_threads
+  $t1 = Thread.new {
+    $feed1 = FeedTools::Feed.open('http://legendas.tv/rss-destaques-series.html')
+  }
 
-#for item in feed.items
-##  puts item.title
-##  puts item.link
-#  puts item.content
-#end
+  $t2 = Thread.new {
+    $feed2 = FeedTools::Feed.open('http://twitter.com/statuses/user_timeline/20011969.rss')
+  }
+end
+
+run_threads
+while $t1.alive? and $t2.alive?
+  sleep 1
+end
+
+feed = nil
+unless $t1.alive?
+  puts "Feed do Legendas.tv respondeu primeiro."
+  feed = $feed1
+else
+  puts "Feed do Twitter respondeu primeiro."
+  feed = $feed2
+end
 
 LIMITE = 116
 
 def envia msg
   msg.delete!("legendas: ")
   if msg.size > LIMITE
-      puts "Tamanho ultrapassou o limite. Truncando pra caber..."
-      msg.slice!(0...LIMITE)
-    end
-    sms = SMS::trata_caracteres_invalidos(msg)
-    sms = SMS::enviar(Celular.new(85, 88041544), msg)
-    puts sms
+    puts "Tamanho ultrapassou o limite. Truncando pra caber..."
+    msg.slice!(0...LIMITE)
+  end
+  sms = SMS::trata_caracteres_invalidos(msg)
+  sms = SMS::enviar(Celular.new(85, 87954849), msg)
+  puts sms
 end
 
+puts "Lendo RSS..."
 feed.items.each do |post|
-  if post.content =~ /Lost/i
+  if post.content =~ /How\.I\.Met\.Your\.Mother/i
+    puts "Enviando SMS: #{post.content}"
     envia post.content
   end
 end
