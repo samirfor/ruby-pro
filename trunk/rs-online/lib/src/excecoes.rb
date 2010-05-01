313dfc7bc673b8f43af33c5bad12c33e7a484f50
samirfor <samirfor@73d1711a-9c8a-11de-a1f4-ada230a207ef>
2010-05-01 08:37:30 +0000

    rs-online: fix save_pacote

--- trunk/rs-online/lib/src/excecoes.rb
+++ trunk/rs-online/lib/src/excecoes.rb
@@ -1,8 +1,18 @@
-require "src/twitter"
+$twitter = true
+begin
+  require 'src/twitter'
+rescue Exception => e
+  $twitter = false
+  to_log "Não foi possível carregar o twitter: #{e.message}"
+end
 
 def interrupt
   to_log "\nSinal de interrupção recebido"
   to_log "O programa foi encerrado."
-  RSTwitter.tweet "O programa foi encerrado."
+  if $twitter
+    RSTwitter.tweet "O programa foi encerrado." 
+  else
+    puts "Não foi possível twittar. Veja log para mais detalhes."
+  end
   exit!(1)
 end
