d0f5f9ae6fc1cdaeafacc7c30b689761268b99f0
samirfor <samirfor@73d1711a-9c8a-11de-a1f4-ada230a207ef>
2010-05-04 02:28:11 +0000

    legendas-notifier: add rss_bip

---
+++ legendas-notifier/lib/rss_bip.rb
@@ -0,0 +1,40 @@
+=begin
+
+puts feed.title
+puts feed.link
+puts feed.description
+
+for item in feed.items
+  puts item.title
+  puts item.link
+  puts item.content
+end
+
+=end
+
+
+require 'rubygems'
+require 'feed_tools'
+
+if ARGV[0] == nil or ARGV[0].chomp == ""
+  puts "Qual o nome da série? Ex: ruby #{__FILE__} Smallville"
+  abort
+end
+
+loop do
+
+  feed = FeedTools::Feed.open('http://legendas.tv/rss-destaques-series.html')
+
+  feed.items.each do |post|
+    if post.title =~ /#{ARGV[0].chomp}/i
+      Thread.new {`mpg123 -qo pulse --loop 2 /home/samir/eita_mah.mp3`}
+      puts post.title
+      puts post.link
+      `firefox \"#{post.link}&c=1\"`
+      exit!
+    end
+  end
+
+  puts "#{Time.now}\tNada ainda :\\"
+  sleep 5*60
+end
