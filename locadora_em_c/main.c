686c5b7b23d2a559c7f8cdd3cd22b01d0b2ce18e
samirfor <samirfor@73d1711a-9c8a-11de-a1f4-ada230a207ef>
2010-05-06 21:48:51 +0000

    locadora_em_c created

---
+++ locadora_em_c/main.c
@@ -0,0 +1,42 @@
+/* 
+ * File:   main.c
+ * Author: alunos
+ *
+ * Created on 26 de Abril de 2010, 14:58
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10
+
+/*
+ *  CRUD (Create Recover Update Delete)
+ */
+
+
+
+void main_menu() {
+    char opcao = '\0';
+
+    do {
+        printf("++ MENU ++\n");
+        printf("1 - Listar\n");
+        printf("2 - Inserir\n");
+        printf("3 - Alterar\n");
+        printf("4 - Deletar\n");
+        printf("0 - Sair\n");
+        printf("++++++\n");
+
+        scanf("%c", &opcao);
+
+        switch (opcao) {
+            case '1': list_costumers();
+            case '2': call_insert();
+            case '3': call_update();
+            case '4': call_delete();
+        }
+    } while (opcao != '0');
+    exit(0);
+}
+
