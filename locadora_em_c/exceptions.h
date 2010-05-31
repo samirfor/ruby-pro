67c503bf390947e96e6329280a37a321e0a6f432
samirfor <samirfor@73d1711a-9c8a-11de-a1f4-ada230a207ef>
2010-05-31 07:21:47 +0000

    locadora_em_c: trying implement movie module

---
+++ locadora_em_c/exceptions.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   exceptions.h
+ * Author: samir
+ *
+ * Created on 31 de Maio de 2010, 03:12
+ */
+
+#ifndef _EXCEPTIONS_H
+#define	_EXCEPTIONS_H
+
+/************************************
+ * Error messages
+ ************************************/
+#define READ_OPEN_ERROR "%s: Nao foi possivel abrir \"%s\" para leitura.\n"
+#define FILE_NOT_FOUND_ERROR "%s: Nao foi possivel localizar o arquivo \"%s\".\n"
+#define ALLOC_ERROR "%s: ERRO FATAL -> Nao foi possivel alocar memoria.\n"
+#define WRITE_FILE_ERROR "%s: ERRO FATAL -> Nao foi possivel escrever no arquivo \"%s\".\n"
+#define CREATE_FILE_ERROR "%s: Nao foi possivel criar \"%s\"\n"
+#define ID_NOT_FOUND_ERROR "%s: Nao ha cliente cadastrado com esse ID.\n"
+#define NAME_NOT_FOUND_ERROR "%s: Nao ha cliente cadastrado com esse nome.\n"
+#define FILE_EMPTY_ERROR "%s: Nao ha clientes cadastrados.\n"
+
+#endif	/* _EXCEPTIONS_H */
+
--- locadora_em_c/movie.h
+++ locadora_em_c/movie.h
@@ -3,12 +3,87 @@
  * Author: samir
  *
  * Created on 5 de Maio de 2010, 16:24
+ *
+ * REQUISITOS:
+ *
+ * -> FILME(ID,TITULO,GENERO,DURACAO)
+ * - INSERIR, ALTERAR, EXCLUIR, LISTAR
+ * - *ORDENAR POR TITULO E PESQUISAR POR TITULO.
+ * 
  */
 
 #ifndef _MOVIE_H
 #define	_MOVIE_H
 
+/***********************************
+ * External Variables References
+ ************************************/
 
+typedef struct {
+    int id;
+    char title[150];
+    char genere[100];
+    int lenght;
+} Movie;
+
+/***************************************
+ * Files references
+ ***************************************/
+
+#define MOVIES_ID_FILEPATH "movies_id_seq.bin"
+#define MOVIES_FILEPATH "movies.bin"
+#define MOVIES_TMP_FILEPATH "tmp_movies.bin"
+
+/***********************************
+ * External Function References
+ ************************************/
+
+/* Verifica a existencia de um filme pelo id */
+extern int movie_index_exist(int);
+/* Search into a vector of movie and returns */
+extern Movie * search_movie_by_id(int);
+/* Search into a vector of movie and returns */
+extern Movie * search_movie_by_title(char*);
+/* Retorna um número id disponível */
+extern int movie_first_index_avaliable();
+/* Ordenar arquivo filme por nome  */
+extern Movie * sort_movie_by_title();
+/* Imprime na tela um filme de forma humanamente legível */
+extern void puts_movie(Movie*);
+/* Remove o lixo da estrutura */
+extern void movie_initialize(Movie*);
+/* Declara dinâmicamente um filme */
+extern Movie * movie_malloc();
+/* Verifica se o arquivo MOVIES_FILEPATH está vazio */
+extern int movies_file_is_empty();
+/* Carrega o arquivo com estruturas Movie num array. */
+extern Movie * movie_file_to_a();
+/* Copia filmes. */
+extern void copy_movie(Movie* dest, Movie* src);
+/* Ver a quantidade de filmes no arquivo. */
+extern int get_size_movies();
+/* Lista todos os filmes */
+extern void list_all_movies();
+/* Lista um filme identificado pelo id */
+extern void list_movie_by_id(int);
+/* Insere um filme no arquivo */
+extern int insert_movie(Movie*);
+/* Modifica um filme no arquivo */
+extern int update_movie(Movie*);
+/* Remove um filme no arquivo */
+extern int erase_movie(Movie*);
+/* Formulário com os atributos do filme */
+extern void form_movie(Movie*);
+/* Formulário de inserção de filme */
+extern void form_movie_insert();
+/* Formulário de atualização de filme */
+extern void form_movie_update();
+/* Formulário de pesquisa de filme */
+extern void form_movie_search();
+/* Formulário de remoção de filme */
+extern void form_movie_erase();
+/* Formulário de ordenação de filmes */
+extern void form_movie_sort();
 
 #endif	/* _MOVIE_H */
 
