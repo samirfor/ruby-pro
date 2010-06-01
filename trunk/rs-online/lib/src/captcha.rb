cc52e7d3d61417f649a553a81ffc2051df72e9b9
samirfor <samirfor@73d1711a-9c8a-11de-a1f4-ada230a207ef>
2010-06-01 04:46:07 +0000

    rs-online: MU implementment in curse.

--- trunk/rs-online/lib/src/captcha.rb
+++ trunk/rs-online/lib/src/captcha.rb
@@ -4,10 +4,18 @@
 module Captcha
   # App GOCR is necessary
   def self.recognize(path)
-    system("convert #{path} -colorspace Gray #{path}")
-    system("convert #{path} #{path.gsub(".png", "")}.tiff")
+    name_no_ext = "#{path.gsub(/\..+/, "")}"
+    new_path = "#{path.gsub(".gif", "")}.tif"
+    txt_path = "#{path.gsub(".gif", "")}.txt"
+    puts "convert -separate #{path} #{new_path}"
+    system "convert -separate #{path} #{new_path}"
+    puts "tesseract #{new_path} #{txt_path}"
+    system "tesseract #{new_path} #{name_no_ext}"
+    captcha = `grep -m 1 . #{txt_path}`.strip
+    captcha.delete(" ").gsub(/\W/, "")
+#    system("convert #{path} -colorspace Gray #{path}")
 #    system("convert #{path} -negate #{path}")
-    `gocr #{path}.tiff`.chomp.delete(" ").strip
+#    `gocr #{path}.tiff`.chomp.delete(" ").strip
   end
 
   # App WGET is necessary
