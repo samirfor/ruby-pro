33db0ab915fc07681896c984bc200c04dde0d81f
samirfor <samirfor@73d1711a-9c8a-11de-a1f4-ada230a207ef>
2010-06-04 23:42:44 +0000

    rs-online: implementing 4shared

--- rs-online/lib/src/pacote.rb
+++ rs-online/lib/src/pacote.rb
@@ -1,7 +1,7 @@
 require "src/timestamp"
 require "rubygems"
 require "shorturl"
-require "src/banco"
+require "src/link"
 
 class Pacote
   attr_accessor :id_pacote, :tamanho, :problema, :nome, :completado, \
@@ -79,25 +79,25 @@ class Pacote
 
   # Retorna o pacote a ser baixado mais prioritário e mais recente.
   # Retorno: Objeto Pacote
-  def select_pendente
+  def self.select_pendente
     sql = "SELECT id, nome, MAX(prioridade) AS prioridade_max " +
       "FROM rs.pacote WHERE completado = 'false' AND problema = 'false' " +
       "GROUP BY id, nome, prioridade ORDER BY prioridade desc, id desc LIMIT 1"
-    rst = Banco.instance.db_connect.execute(sql)
     begin
+      rst = Banco.instance.db_connect.execute(sql)
+      pacote = nil
       rst.fetch do |row|
-        @nome = row["nome"]
-        @id_pacote = row["id"]
-        @prioridade = row["prioridade_max"]
+        pacote = Pacote.new(row["nome"])
+        pacote.id_pacote = row["id"]
+        pacote.prioridade = row["prioridade_max"]
       end
     rescue Exception => err
-      puts "Erro no fetch"
-      puts err
-      @id_pacote = nil
+      puts "Erro no fetch ou resultset: #{err}"
+      nil
     end
     rst.finish
-
     Banco.instance.db_disconnect
+    pacote
   end
 
   # Tras do banco o pacote com o id específico
@@ -197,4 +197,126 @@ class Pacote
     return retorno
   end
 
+  # Captura os pacotes que estão para ser baixado com exceção de um, o qual
+  # é passado como parâmetro.
+  # Retorno: Objeto Pacote
+  def self.select_pacotes_pendetes_teste id_pacote
+    sql = "SELECT id, nome, MAX(prioridade) AS prioridade_max " +
+      "FROM rs.pacote WHERE completado = 'false' AND problema = 'false' " +
+      "AND id != ? GROUP BY id, nome, prioridade " +
+      "ORDER BY prioridade desc, id desc"
+    rst = Banco.instance.db_connect.execute(sql, id_pacote)
+    pacotes = Array.new
+    pacote = nil
+    begin
+      rst.fetch do |row|
+        pacote = Pacote.new(row["nome"])
+        pacote.id_pacote = row["id"]
+        pacote.prioridade = row["prioridade_max"]
+        pacotes.push pacote
+      end
+    rescue Exception => err
+      puts "Erro no fetch: #{err}"
+      nil
+    end
+    rst.finish
+    Banco.instance.db_disconnect
+    pacotes
+  end
+
+  def self.select_lista mostrar
+    sql = "SELECT * FROM rs.pacote WHERE mostrar = ? "
+    begin
+      rst = Banco.instance.db_connect.execute(sql, mostrar)
+      pacotes = Array.new
+      pacote = nil
+      rst.fetch do |row|
+        pacote = Pacote.new(row["nome"])
+        pacote.id_pacote = row["id"]
+        pacote.completado = row["completado"]
+        pacote.mostrar = row["mostrar"]
+        pacote.problema = row["problema"]
+        pacote.data_inicio = row["data_inicio"]
+        pacote.data_fim = row["data_fim"]
+        pacote.senha = row["senha"]
+        pacote.prioridade = row["prioridade"]
+        pacote.tamanho = row["tamanho"]
+        pacote.descricao = row["descricao"]
+        pacote.url_fonte = row["url_fonte"]
+        pacote.legenda = row["legenda"]
+        pacotes.push pacote
+      end
+    rescue Exception => err
+      puts "Erro no fetch"
+      puts err
+      pacotes = nil
+    end
+    rst.finish
+    Banco.instance.db_disconnect
+    return pacotes
+  end
+  
+  # Captura a lista de links.
+  # Retorna um Array de Objetos Link
+  def select_links
+    if @id_pacote == nil or @id_pacote == ""
+      return nil
+    end
+=begin
+  id_link bigserial NOT NULL,
+  id_pacote integer NOT NULL,
+  link character varying(300) NOT NULL,
+  completado boolean DEFAULT false,
+  tamanho integer,
+  id_status integer NOT NULL DEFAULT 5,
+  data_inicio timestamp without time zone,
+  data_fim timestamp without time zone,
+  testado boolean NOT NULL DEFAULT false,
+=end
+    sql = "SELECT l.id_link, l.id_pacote, l.link, l.completado, l.tamanho, "
+    sql += "l.id_status, l.data_inicio, l.data_fim, l.testado "
+    sql += "FROM rs.pacote p, rs.link l "
+    sql += "WHERE l.id_pacote = p.id AND p.id = ? "
+    lista = Array.new
+    link = nil
+    begin
+      rst = Banco.instance.db_connect.execute(sql, @id_pacote)
+      rst.fetch do |row|
+        link = Link.new(row["link"])
+        link.id_link = row["id_link"]
+        link.id_pacote = row["id_pacote"]
+        link.completado = row["completado"]
+        link.tamanho = row["tamanho"]
+        link.id_status = row["id_status"]
+        link.testado = row["testado"]
+        link.data_inicio = row["data_inicio"]
+        link.data_fim = row["data_fim"]
+        lista.push link
+      end
+      rst.finish
+      Banco.instance.db_disconnect
+      lista.sort
+    rescue Exception => e
+      to_log "Houve erro => #{e}"
+      sleep 1
+      return nil
+    end
+  end
+
+  # Verifica a quantidade de pacotes e a quantidade de pacotes baixado.
+  # O retorno é a diferença entre as respectivas quantidades.
+  def select_count_remaining_links
+    sql = "SELECT count(id_link) FROM rs.link WHERE id_pacote = ? "
+    rst = Banco.instance.db_connect.execute(sql, @id_pacote)
+    count_pacotes = rst.fetch_all[0].clone
+    rst.finish
+
+    sql = "SELECT count(id_link) FROM rs.link WHERE id_pacote = ? AND id_status = 1 "
+    rst = @conn.execute(sql, @id_pacote)
+    count_baixados = rst.fetch_all[0].clone
+    rst.finish
+    Banco.instance.db_disconnect
+
+    return count_pacotes - count_baixados
+  end
 end
